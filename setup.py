22d1bd5c71b6d86fdbf88b24c51760a498de4960
lucas-bar <lucas.barbier@edu.devinci.fr>
2024-01-29 17:35:13 +0100

    modification of the setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
+
+from setuptools import setup, find_packages
 from distutils.core import setup
 setup(
- name='{name-of-your-project}',
- version='1.0',
- author='{your-full-name-or-your-github-username}',
- license='MIT',
- long_description=open('README.md').read(),
+    name='todo_list_application_barbier_lucas_5',
+    version='1.0',
+    author='Lucas Barbier',
+    license='MIT',
+    description='A console-based application to help keep track of tasks.',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'todo_list_application = main:main',  # Remplacez 'main' par le nom de votre fichier principal
+        ],
+    },
+    install_requires=[
+        # Liste des dépendances, le cas échéant
+    ],
 )
+
